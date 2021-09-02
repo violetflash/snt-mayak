50b1761d8f384980d721ad990f3919f0389e4a80
Alex Grobovsky <ktotaam@mail.ru>
2021-09-02 15:25:13 +0500

    fix: fix parallax svg's classes

--- src/components/Parallax/GardenerGirl/GardenerGirl.jsx
+++ src/components/Parallax/GardenerGirl/GardenerGirl.jsx
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import Parallax from 'parallax-js';
 
 import s from './GardenerGirl.module.scss';
-// import sprite from "../../../assets/gardener/svgSprite.svg";
 import { ReactComponent as Girl } from "../../../assets/gardener/girl.svg";
 import { ReactComponent as Bushes } from "../../../assets/gardener/bushes.svg";
 import { ReactComponent as Tree } from "../../../assets/gardener/tree-plain.svg";
 import { ReactComponent as Bg } from "../../../assets/gardener/bg.svg";
-
 
 class GardenerGirl extends Component {
     componentDidMount() {
@@ -24,7 +22,8 @@ class GardenerGirl extends Component {
                 <Girl className={s.GardenerGirl__girl} data-depth="0"/>
                 <Bushes className={s.GardenerGirl__bushes} data-depth="0.2"/>
                 <Tree className={s.GardenerGirl__tree} data-depth="0.2"/>
-                {/*<Bg className={s.GardenerGirl__bg} data-depth="0.5"/>*/}
+                <Bg className={s.GardenerGirl__bg} data-depth="0.5"/>
+
                 {/*<img className={s.GardenerGirl__girl} src={sprite + "#girl"} alt="gardener" data-depth="0" />*/}
                 {/*<img className={s.GardenerGirl__bushes} src={sprite + "#bushes"} alt="gardener" data-depth="0.2" />*/}
                 {/*<img className={s.GardenerGirl__tree} src={sprite + "#tree-plain"} alt="gardener" data-depth="0.2" />*/}
---
+++ src/components/SvgSprite/SvgSprite.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as IconSprite } from 'assets/svgs/icon-sprite.svg';
+
+const SvgSprite = () => {
+    return (
+        <div style={{ display: 'none' }}>
+            <IconSprite />
+        </div>
+    );
+};
+
+export default SvgSprite;
